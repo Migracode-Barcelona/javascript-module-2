fd15db2e811a99c2352f26450fac0d33478fdeed
Burçak <89997401+burcakkok@users.noreply.github.com>
2022-04-17 20:25:00 +0200

    week3 homework done

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,31 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+function setAlarm() {
+    let time = document.querySelector("#alarmSet").value;
+    const timeRemaining = document.querySelector("#timeRemaining"); // = Time Remaining: 00:00;
+    timeRemaining.textContent = "Time Remaining: 00:" + time;
+  
+    const intervalId = setInterval(() => {
+      time = time - 1;
+      timeRemaining.textContent = "Time Remaining: 00:" + time;
+  
+      if (time === 0) {
+        clearInterval(intervalId);
+        playAlarm();
+      }
+    }, 5000);
+};
+
+const body = document.querySelector("body");
+console.log(body);
+body.style.background = "salmon";
+
+setInterval(() => {
+  if (body.style.background === "skyblue") {
+    body.style.background = "salmon";
+  } else {
+    body.style.background = "skyblue";
+  }
+}, 5000);
--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -1,1 +1,60 @@
 // Write your code here
+const img1 = "https://images.unsplash.com/photo-1620766567920-ccb8cc5c8a87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80";
+const img2 = "https://images.unsplash.com/photo-1583339835381-8f89b73c346e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=354&q=80";
+const img3 = "https://images.unsplash.com/photo-1641490238587-6b35678fb28b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+const img4 = "https://images.unsplash.com/photo-1599311979600-a629977414ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+const img5 = "https://images.unsplash.com/photo-1647717830880-97983506c007?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+
+const imagesOfCoolAnimals = [img1, img2, img3, img4, img5];
+
+let imgIndex = 0;
+let intervalVar;
+let delayInterval = 2000;
+
+const autoBackBtn = document.getElementById("auto-back-btn");
+const backBtn = document.getElementById("back-btn");
+const stopBtn = document.getElementById("stop-btn");
+const forwardBtn = document.getElementById("forward-btn");
+const autoForwardBtn = document.getElementById("auto-forward-btn");
+const selectImage = document.getElementById("random-image");
+
+autoBackBtn.addEventListener('click', autoBackEvent);
+backBtn.addEventListener('click', backEvent);
+stopBtn.addEventListener('click', stopEvent);
+forwardBtn.addEventListener('click', forwardEvent);
+autoForwardBtn.addEventListener('click', autoForwardEvent);
+
+function showImage() {
+  selectImage.src = imagesOfCoolAnimals[imgIndex];
+}
+
+function backEvent() {
+  imgIndex = imgIndex == 0 ? imagesOfCoolAnimals.length - 1 : imgIndex - 1;
+  showImage();
+}
+
+function forwardEvent() {
+  imgIndex = imgIndex == imagesOfCoolAnimals.length - 1 ? 0 : imgIndex + 1;
+  showImage();
+}
+
+function autoBackEvent() {
+  resetInt(backEvent);
+}
+
+function autoForwardEvent() {
+  resetInt(forwardEvent);
+}
+
+function stopEvent() {
+  resetInt();
+}
+
+function resetInt(fnc) {
+  if (intervalVar) clearInterval(intervalVar);
+  if (typeof fnc === "function") {
+    intervalVar = setInterval(fnc, delayInterval);
+  }
+}
+
+showImage();
