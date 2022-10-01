c42aa63eb93d3398fb39e9b1253d5f4fa983d62b
Yun Ji <this.jiyun@gmail.com>
2022-10-01 09:07:55 +0200

    Finished exercises A

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,28 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+//task 1
+
+setTimeout(() => {
+    document.querySelector("body").style.backgroundColor = 'purple'
+
+}, 5000)
+
+
+//task 2
+
+const changeColor = () => {
+    let bodyStyle = document.querySelector('body').style
+    let colors = ['red', 'purple', 'green', 'pink', 'blue', 'black']
+    let index = Math.round(Math.random() * 10)
+    if (index >= 0 && index <= 5) {
+        bodyStyle.backgroundColor = colors[index]
+    }
+
+}
+
+setInterval(() => {
+    changeColor()
+}, 5000)
+
